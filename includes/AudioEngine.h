3e8f9d788c90aa580210fe565e8c7ce2393d6a83
Alexander von Gluck IV <kallisti5@unixzen.com>
2010-04-15 15:32:12 +0000

    added AudioEngine class which will handle audio playback management

---
+++ includes/AudioEngine.h
@@ -0,0 +1,33 @@
+/*
+ * Freebird, the Music Jukebox for Haiku
+ * 2010, Alexander von Gluck
+ * http://unixzen.com, http://github.com/kallisti5
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _AUDIO_ENGINE_H
+#define _AUDIO_ENGINE_H
+
+#include <SoundPlayer.h>
+
+
+class AudioEngine {
+	public:
+		bool	IsPlaying() { return isPlaying; };
+	private:
+		bool	isPlaying;
+};
+
+#endif
